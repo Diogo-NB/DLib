6ea77716b9e8ddd3c3cab239e03e62a5661901aa
Diogo-NB <113485393+Diogo-NB@users.noreply.github.com>
2024-01-08 03:10:07 -0300

    Add comments to avl_tree functions

--- avl_tree_lib/avl_tree.h
+++ avl_tree_lib/avl_tree.h
@@ -23,18 +23,25 @@ struct avl_tree
 
 typedef struct avl_tree *AVL_Tree;
 
+// Allocates memory for a new tree
 AVL_Tree create_avl_tree(int (*comparef)(void *, void *));
 
+// Returns true (1) if the list is empty
 int avl_is_empty(AVL_Tree T);
 
+// Allocates memory for a new node
 AVL_Node create_avl_node(void *data);
 
+// Deallocates tree
 void free_avl_tree(AVL_Tree T);
 
+// free_avl_tree while calling a free data function for each element
 void free_avl_tree_func(AVL_Tree T, void (*free_data_func)(void *));
 
+// Inserts a new data into the tree
 void avl_insert(AVL_Tree T, void *data);
 
+// Searchs the tree and returns a node's data where comparef returns 0
 void *avl_search(AVL_Tree T, void *data);
 
 void avl_pre_order(AVL_Tree T, void (*func)(void *));
